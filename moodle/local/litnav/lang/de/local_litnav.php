f92935ac4ff788e707beb2f8108a353b9810db35
Frederik Strelczuk <frederik.strelczuk@gmail.com>
2013-06-14 21:21:36 +0200

    Added local-plugin for navigation

---
+++ moodle/local/litnav/lang/de/local_litnav.php
@@ -0,0 +1,9 @@
+<?php
+
+$string['pluginname'] = 'Naviagtionserweiterung für mod_literature';
+
+$string['importlists'] = 'Liste importieren';
+$string['literature'] = 'Literatur';
+$string['managelists'] = 'Listen verwalten';
+$string['managesource'] = 'Suchquellen verwalten';
+$string['searchlit'] = 'Literatur suchen';
