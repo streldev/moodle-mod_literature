1dac0b0ec6adb1a4eac699c1b97c559aa17c8047
Frederik Strelczuk <frederik.strelczuk@gmail.com>
2014-07-22 11:18:21 +0200

    Removed .gitignore and added some files

---
+++ lang/de/literature.php
@@ -0,0 +1,216 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * German strings for literature
+ *
+ * @package    mod_literature_lang
+ * @subpackage de
+ * @copyright  2012 Frederik Strelczuk
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+
+$string['modulename'] = 'Literatur';
+$string['modulenameplural'] = 'Literatures';
+$string['modulename_help'] = 'Das Literatur-Modul erlaubt es Literaturdaten in verschiedenen Quellen zu suchen, zu verwalten und in Kursen zu veröffentlichen.';
+$string['literature'] = 'literature';
+$string['pluginadministration'] = 'literature administration';
+$string['pluginname'] = 'Literatur';
+
+
+$string['book'] = 'Buch';
+$string['electronic'] = 'Elektronische Ressource';
+$string['misc'] = 'Sonstiges';
+
+
+$string['actions'] = 'Aktionen';
+$string['actionsel'] = 'Wähle Aktion';
+$string['add'] = 'Hinzufügen';
+$string['addlist'] = 'Liste hinzufügen';
+$string['addlit'] = 'Literatur hinzufügen';
+$string['addsource'] = 'Suchquele hinzufügen';
+$string['addtolist'] = 'Zu Liste hinzufügen';
+$string['addtonewlist'] = 'Zu neuer Liste hinzufügen';
+$string['authors'] = 'Autoren: ';
+
+// ---------------------------------------------------------------------------------------
+// Search connectors
+$string['and'] = 'und';
+$string['or'] = 'oder';
+$string['andnot'] = 'und nicht';
+
+$string['cover'] = 'Cover';
+$string['created:'] = 'Erstellt: ';
+$string['delete'] = 'Löschen';
+$string['description'] = 'Beschreibung';
+$string['description:'] = 'Beschreibung: ';
+$string['edit'] = 'Bearbeiten';
+$string['empty'] = 'Leer';
+$string['export'] = 'Export';
+$string['exportset'] = 'Exporteinstellungen';
+$string['extendedsearch'] = 'Erweiterte Suche';
+$string['filename'] = 'Dateiname';
+$string['files'] = 'Dateien';
+$string['format'] = 'Format';
+$string['format:'] = 'Format: ';
+$string['import'] = 'Import';
+$string['importlist'] = 'Liste importieren';
+$string['importlit'] = 'Literatur importieren';
+$string['inonefile'] = 'In einzelne Datei exportieren';
+$string['isactiveenricher'] = 'Gibt an, ob der Enricher aktiv ist.';
+$string['isbn'] = 'ISBN: ';
+$string['isbn10'] = 'ISBN10: ';
+$string['isbn13'] = 'ISBN13: ';
+$string['issn'] = 'ISSN: ';
+$string['ispublic'] = 'Öffentlich';
+$string['items'] = 'Einträge';
+$string['link'] = 'Link: ';
+$string['links'] = 'Links';
+$string['linktext'] = 'Linktext: ';
+$string['listinfo'] = 'Listinfo';
+$string['lists'] = 'Listen';
+$string['lit'] = 'Literatur';
+$string['litinfo'] = 'Literaturinformation';
+$string['littype'] = 'Typ: ';
+$string['load'] = 'Anzeigen';
+$string['loadform'] = 'Formular anzeigen';
+$string['managelists'] = 'Verwalte Listen';
+$string['managelit'] = 'Verwalte Literatur';
+$string['managesources'] = 'Verwalte Suchquellen';
+$string['name'] = 'Name: ';
+$string['new_list'] = 'Neue Liste';
+$string['nextresults'] = 'Nächste Ergebnisse';
+$string['nolist'] = 'Liste enthält keine Einträge';
+$string['nolists'] = 'Keine Listen gefunden';
+$string['noresults'] = 'Keine Ergebnisse';
+$string['nosource'] = 'Keine Suchquelle installiert';
+$string['options'] = 'Optionen';
+$string['postas'] = 'Veröffentlichen als';
+$string['postlists'] = 'Auswahl veröffentlichen';
+$string['post'] = 'Veröffentlichen';
+$string['prevresults'] = 'Vorherhige Ergebnisse';
+$string['published'] = 'Veröffentlicht: ';
+$string['publisher'] = 'Verlag: ';
+$string['quicksearch'] = 'Schnellsuche';
+$string['results'] = 'Ergebnisse';
+$string['save'] = 'Speichern';
+$string['saveandsearch'] = 'Speichern und suchen';
+$string['search'] = 'Suchen';
+$string['searchlit'] = 'Literatur suchen';
+$string['search_results'] = 'Suchergebnisse';
+$string['sel_source'] = 'Suchquelle';
+$string['srctype'] = 'Typ';
+$string['selecttype'] = 'Typ auswählen';
+$string['settings'] = 'Einstellungen';
+$string['series'] = 'Reihe: ';
+$string['source'] = 'Quelle';
+$string['sourceoverview'] = 'Suchquellenübersicht';
+$string['sources'] = 'Suchquellen';
+$string['srctype'] = 'Suchquellentyp';
+$string['subtitle'] = 'Untertitel: ';
+$string['text'] = 'Text';
+$string['title'] = 'Titel: ';
+$string['titlelink'] = 'Titellink: ';
+$string['title_add_list'] = 'Neue Literaturliste';
+$string['title_edit_list'] = 'Bearbeite  Literaturliste';
+$string['title:exportlists'] = 'Liste exportieren';
+$string['title:exportlit'] = 'Literatur exportieren';
+$string['title:listoverview'] = 'Listenübersicht';
+$string['type'] = 'Typ: ';
+$string['url'] = 'URL: ';
+$string['view_list'] = 'Liste anzeigen';
+$string['view_as_list'] = 'Liste';
+$string['view_as_full'] = 'Vollansicht';
+
+
+// HELP
+
+$string['help:addlist:public'] = 'Öffentlich';
+$string['help:addlist:public_help'] = 'Wenn die Liste als öffentlich gekennzeichnet wurde, können alle Benutzer die Liste exportieren.';
+
+$string['help:exportlists:inonefile'] = 'In einzelne Datei exportieren?';
+$string['help:exportlists:inonefile_help'] = 'Wenn diese Option aktiviert wurde, werden die ausgewählten Listen in eine einzelne Datei exportiert.' .
+        'Ansonsten wird pro ausgewählter Liste eine neue Datei erstellt.';
+
+
+// ERRORS
+$string['error:noterm'] = 'Ohne Suchterm keine Suche! :-)';
+$string['error:nolistselected'] = 'Keine Liste ausgewählt!';
+$string['error:nolitselected'] = 'Keine Literatur ausgewählt!';
+$string['error:novalidlist'] = 'Keine gültige Liste ausgewählt!';
+$string['error:novalidaction'] = 'Keine gültige Aktion ausgewählt!';
+
+$string['error:db:addlit2list'] = 'Die Literatur mit dem Titel: {$a->title} konnte nicht zur Liste hinzugefügt werden.';
+$string['error:db:incorectlinkhandling'] = 'Gratulation! Sie haben einen Fehler im Linkmanagement der Listen gefunden. Bitte kontaktieren sie den Entwickler des Plugins :-)';
+$string['error:db:litinsert'] = 'Die Literatur mit Titel: {$a->title} konnte nicht in die Datenbank eingefügt werden.';
+$string['error:db:litnotfound'] = 'Die Literatur mit der ID: {$a} konnte nicht in der Datenbank gefunden werden.';
+$string['error:db:litupdate'] = 'Das Update der Literatur mit der ID: {$a} konnte nicht durchgeführt werden.';
+
+$string['error:enricher:classnotfound'] = 'Jetzt wird es unheimlich. Eine Sekunde zuvor wurde die gesuchte Datei noch gefunden.' .
+        'Jetzt scheint die Datein und damit die Klasse des Enricher zu fehlen. Hat sie jemand gelöscht?';
+$string['error:enricher:notinstalled'] = 'Der Enricher {$a} ist nicht installiert. Um ihn zu nutzen, muss das entsprechende Subplugin installiert werden.';
+
+$string['error:export:nolists'] = 'Keine Liste für den Export ausgewählt. Wurde die Session beendet?';
+
+$string['error:exporter:couldnotcreatefile'] = 'Die Datei für den Export konnte nicht geschlossen werden. Kontaktieren sie den Moodel-Administrator.';
+$string['error:exporter:export'] = 'Unbekannter Fehler beim Export!';
+$string['error:exporter:notinstalled'] = 'Das Exportformat {$a} ist nicht installiert!';
+
+$string['error:file:emptycontent'] = 'Es scheint, dass die Datei {$a} keinen Inhalt hat!?';
+$string['error:file:getafterupload'] = 'Das Laden der hochgeladenen Datei {$a->filename} ist fehlgeschlagen!';
+
+$string['error:importer:extensionnotsupported'] = 'Das Importformate für die Dateiendung {$a->yourextension} ist nicht installiert. Unterstützt werden: {$a->extensions}';
+$string['error:importer:import'] = 'Der Import der Datei {$a} ist fehlgeschlagen!';
+$string['error:importer:notinstalled'] = 'Das Importformat {$a} ist nicht installiert!';
+
+$string['error:list:accessdenied'] = 'Sie haben keine Erlaubnis auf diese Liste zu zugreiffen!';
+$string['error:list:entriesmissing'] = 'Während dem Laden der Liste wurden {$a} fehlweisende Referenzen in der Liste gefunden und gelöscht. Versuchen Sie es erneut.';
+$string['error:list:insert'] = 'Das Einfügen der Liste {$a} in die Datenbank ist fehlgeschlagen.';
+$string['error:list:loadfailed'] = 'Das Laden der Liste mit der ID: {$a->listid} ist fehlgeschlagen!';
+
+$string['error:list:import:fileload'] = 'Das Laden der Datei {$a} schlug fehl.';
+
+$string['error:lit:loadfailed'] = 'Das Laden der Literatur mit der ID: {$a} ist fehlgeschlagen!';
+
+$string['error:post:litfailedcm'] = 'Die Literatur mit der ID: {$a->id} konnte nicht veröffentlicht werden.' .
+        'Grund: Der entsprechende Eintrag in die Datenbanktabelle course_module war nicht erfolgreich!';
+
+$string['error:search:saveresultsfailed'] = 'Das Speichern der Ergebnisse in der Datenbank ist fehlgeschlagen!';
+$string['error:search:timestampnotfound'] = 'Das Laden der Ergebnisse aus der Datenbank ist fehlgeschlagen! Versuchen Sie erneut zu suchen.';
+$string['error:search:couldnotloadresult'] = 'Das Suchergebnis wurde nicht mehr in der Datenbank gefunden. Bitte suchen sie erneut.';
+
+$string['error:searchsource:dbobjectmissing'] = 'Das dbobject der Suchquelle konnte nicht gefunden werden. Installieren sie das dazugehörige Subplugin erneut oder löschen' .
+        ' sie den Eintrag der Suchquelle von Hand in der Datenbank.';
+$string['error:searchsource:formnotfound'] = 'Das Formular für die gewählte Suchquelle konnte nicht gefunden werden!';
+$string['error:searchsource:functionmakeformdata'] = 'Die gewählte Suchquelle ist fehlerhaft. Die Funktion {$a} ist nicht implementiert!';
+$string['error:searchsource:lib'] = 'Fehler beim Laden der Bibliothek für die Suchquellen!';
+$string['error:searchsource:noinstalled'] = 'Noch keine Suchquelle installiert! Bitten sie den Administrator eine Suchquelle zu installieren.';
+$string['error:searchsource:notinstalled'] = 'Suchquelle {$a} ist nicht installiert. Suchobjekt nicht gefunden!';
+$string['error:searchsource:confignotfound'] = 'Die angeforderte Konfiguration der Suchquelle existiert nicht!';
+$string['error:searchsource:configrefnotfound'] = 'Der Verweis auf die angeforderte Konfiguration der Suchquelle existiert nicht!';
+$string['error:searchsource:typemissing'] = 'Fehler beim Auswerten des Suchformulars. Der Typ der Suchqulle fehlt!';
+$string['error:searchsource:addfailed'] = 'Anlegen der neuen Suchquelle fehlgeschlagen!';
+
+$string['error:session:nolists'] = 'Keine Listen ausgewählt. Ein anderer Grund kann das Überschreiben der Session sein.';
+
+// NOTIFY
+$string['notify:failedtoloadsearchform'] = 'Das Laden des Suchformulars ist fehlgeschlagen!';
+$string['notify:nolistselected'] = 'Keine Liste ausgewählt!';
+$string['notify:nolitselected'] = 'Keine Literatur ausgewählt!';
+$string['notify:novalidaction'] = 'Keine gültige Operation ausgewählt!';
---
+++ lang/en/literature.php
@@ -0,0 +1,212 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for literature
+ *
+ * @package    mod_literature_lang
+ * @subpackage en
+ * @copyright  2012 Frederik Strelczuk
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+
+$string['modulename'] = 'Literature';
+$string['modulenameplural'] = 'Literatures';
+$string['modulename_help'] = 'The literature module allows to manage and publish literature data.';
+$string['literature'] = 'literature';
+$string['pluginadministration'] = 'literature administration';
+$string['pluginname'] = 'Literature';
+
+
+$string['book'] = 'Book';
+$string['electronic'] = 'Electronic';
+$string['misc'] = 'Misc';
+
+
+$string['actions'] = 'Actions';
+$string['actionsel'] = 'Select action';
+$string['add'] = 'Add';
+$string['addlist'] = 'Add List';
+$string['addlit'] = 'Add Literature';
+$string['addsource'] = 'Add Source';
+$string['addtolist'] = 'Add to list';
+$string['addtonewlist'] = 'Add to new list';
+$string['authors'] = 'Authors: ';
+
+// ---------------------------------------------------------------------------------------
+// Search connectors
+$string['and'] = 'and';
+$string['or'] = 'or';
+$string['andnot'] = 'and not';
+
+$string['cover'] = 'Cover';
+$string['created:'] = 'Created: ';
+$string['delete'] = 'Delete';
+$string['description'] = 'Description';
+$string['description:'] = 'Description: ';
+$string['edit'] = 'Edit';
+$string['empty'] = 'Empty';
+$string['export'] = 'Export';
+$string['exportset'] = 'Export settings';
+$string['extendedsearch'] = 'Extended search';
+$string['filename'] = 'Filename';
+$string['files'] = 'Files';
+$string['format'] = 'Format';
+$string['format:'] = 'Format: ';
+$string['import'] = 'Import';
+$string['importlist'] = 'Import List';
+$string['importlit'] = 'Import Literature';
+$string['inonefile'] = 'Export in single file';
+$string['isactiveenricher'] = 'Setting to activate/deactivate the enricher.';
+$string['isbn'] = 'ISBN: ';
+$string['isbn10'] = 'ISBN10: ';
+$string['isbn13'] = 'ISBN13: ';
+$string['issn'] = 'ISSN: ';
+$string['ispublic'] = 'Is public';
+$string['items'] = 'Items';
+$string['link'] = 'Link: ';
+$string['links'] = 'Links';
+$string['linktext'] = 'Text: ';
+$string['listinfo'] = 'Listinfo';
+$string['lists'] = 'Lists';
+$string['lit'] = 'Literature';
+$string['litinfo'] = 'Literature info';
+$string['littype'] = 'Type: ';
+$string['load'] = 'Load';
+$string['loadform'] = 'Load form';
+$string['managelists'] = 'Manage Lists';
+$string['managelit'] = 'Manage Literature';
+$string['managesources'] = 'Manage Sources';
+$string['name'] = 'Name: ';
+$string['new_list'] = 'New list';
+$string['nextresults'] = 'Next Results';
+$string['nolist'] = 'List is empty';
+$string['nolists'] = 'No lists yet';
+$string['noresults'] = 'No Results';
+$string['nosource'] = 'No source installed';
+$string['options'] = 'Options';
+$string['postas'] = 'Post as';
+$string['postlists'] = 'Post selected list(s)';
+$string['post'] = 'Post';
+$string['prevresults'] = 'Prev. Results';
+$string['published'] = 'Published: ';
+$string['publisher'] = 'Publisher: ';
+$string['quicksearch'] = 'QuickSearch';
+$string['results'] = 'Results';
+$string['save'] = 'Save';
+$string['saveandsearch'] = 'Save and search';
+$string['search'] = 'Search';
+$string['searchlit'] = 'Search Literature';
+$string['search_results'] = 'Search results';
+$string['sel_source'] = 'Search source';
+$string['selecttype'] = 'Select type';
+$string['settings'] = 'Settings';
+$string['series'] = 'Series: ';
+$string['source'] = 'Source';
+$string['sourceoverview'] = 'Source overview';
+$string['sources'] = 'Sources';
+$string['srctype'] = 'Source type';
+$string['subtitle'] = 'Subtitle: ';
+$string['text'] = 'Text';
+$string['title'] = 'Title: ';
+$string['titlelink'] = 'Title link: ';
+$string['title_add_list'] = 'New literature list';
+$string['title_edit_list'] = 'Edit literature list';
+$string['title:exportlists'] = 'Export Lists';
+$string['title:exportlit'] = 'Export Literature';
+$string['title:listoverview'] = 'List overview';
+$string['type'] = 'Type: ';
+$string['url'] = 'URL: ';
+$string['view_list'] = 'View List';
+$string['view_as_list'] = 'List';
+$string['view_as_full'] = 'Full view';
+
+
+// HELP
+
+$string['help:addlist:public'] = 'Should list be public?';
+$string['help:addlist:public_help'] = 'If selected, the list is marked as public. Every user can export a public list.';
+
+$string['help:exportlists:inonefile'] = 'Export in one file?';
+$string['help:exportlists:inonefile_help'] = 'If active, the selecteded lists are exported in a single file.';
+
+
+// ERRORS
+$string['error:noterm'] = 'Searching without a term is impossible!!!';
+$string['error:nolistselected'] = 'No list selected!';
+$string['error:nolitselected'] = 'No literature selected!';
+$string['error:novalidlist'] = 'No valid list selected!';
+$string['error:novalidaction'] = 'No valid action selected!';
+
+$string['error:db:addlit2list'] = 'Literature with title: {$a->title} could not be added to list.';
+$string['error:db:incorectlinkhandling'] = 'Congrats! You found a bug in the linkhandling for lists. Please contact the developer :-)';
+$string['error:db:litinsert'] = 'Literature with title: {$a->title} could not be inserted into database.';
+$string['error:db:litnotfound'] = 'Literature with id: {$a} was not found in database!';
+$string['error:db:litupdate'] = 'It was impossible to update the literature with id: {$a}';
+
+$string['error:enricher:classnotfound'] = 'This is realy mysterious. A second ago i found the correct file but now the class of the enricher is missing! Did someone delete the enricher a second ago?';
+$string['error:enricher:notinstalled'] = 'Enricher {$a} is not installed. Please install.';
+
+$string['error:export:nolists'] = 'No lists selected for export or your SESSION is not set correct!';
+
+$string['error:exporter:couldnotcreatefile'] = 'Could not create file for export. Pleas contact the moodle admin!';
+$string['error:exporter:export'] = 'Failed to export!';
+$string['error:exporter:notinstalled'] = 'The export format {$a} is not installed!';
+
+$string['error:file:emptycontent'] = 'It seems the file {$a} doesn`t have any content!';
+$string['error:file:getafterupload'] = 'Can`t get your uploaded file {$a->filename}!';
+
+$string['error:importer:extensionnotsupported'] = 'The extension {$a->yourextension} is not supported. Supported: {$a->extensions}';
+$string['error:importer:import'] = 'Failed to import {$a}!';
+$string['error:importer:notinstalled'] = 'The import format {$a} is not installed!';
+
+$string['error:list:accessdenied'] = 'You don`t have permission to access this list!';
+$string['error:list:entriesmissing'] = 'While loading your list we found {$a} missguiding links in your list and deleted them. Pleas try again.';
+$string['error:list:insert'] = 'Error while inserting list {$a} into database';
+$string['error:list:loadfailed'] = 'Failed to load list with id: {$a->listid}!';
+
+$string['error:list:import:fileload'] = 'Sorry i failed to load file {$a}';
+
+$string['error:lit:loadfailed'] = 'Failed to load literature with id: {$a}!';
+
+$string['error:post:litfailedcm'] = 'Could not post literature with id: {$a->id}. I was not able to add course module entry!';
+
+$string['error:search:saveresultsfailed'] = 'Failed to save the results in the database!';
+$string['error:search:timestampnotfound'] = 'Failed to load results from the database. Please search again.';
+$string['error:search:couldnotloadresult'] = 'The search result could not be found in the db. Please search again.';
+
+$string['error:searchsource:dbobjectmissing'] = 'Failed to load the dbobject of the searchsource you want to delete.' .
+        'Reinstall the coresponding subplugin or delete the entry by yourself.';
+$string['error:searchsource:formnotfound'] = 'Form for selected searchsource not found!';
+$string['error:searchsource:functionmakeformdata'] = 'Searchsource incorrect installed. Function {$a} is missing!';
+$string['error:searchsource:lib'] = 'Failed to load searchsource lib!';
+$string['error:searchsource:noinstalled'] = 'No searchsource is installed! Please contact your moodle admin.';
+$string['error:searchsource:notinstalled'] = 'Searchsource {$a} is not properly installed. Searchobject not found!';
+$string['error:searchsource:confignotfound'] = 'The configuration you asked for does not exist in db!';
+$string['error:searchsource:configrefnotfound'] = 'The reference for the configuration you asked for does not exist in db!';
+$string['error:searchsource:typemissing'] = 'Error while processing search form. Type of the searchsource is missing!';
+$string['error:searchsource:addfailed'] = 'Error while adding new searchsource!';
+
+$string['error:session:nolists'] = 'SESSION is empty. You have to select at least one list.';
+
+// NOTIFY
+$string['notify:failedtoloadsearchform'] = 'Failed to load searchform!';
+$string['notify:nolistselected'] = 'No list selected!';
+$string['notify:nolitselected'] = 'No lit selected!';
+$string['notify:novalidaction'] = 'No valid action was selected!';
